ef5c9169ec1970ea8768f1009753c92d5e659648
Кирилл Фролов <55494281+FrolovKir@users.noreply.github.com>
2019-09-25 17:15:14 +0300

    Update and rename Svetofor.ino to Svetofor_minimum.ino

--- Svetofor_minimum.ino
+++ Svetofor_minimum.ino
@@ -1,30 +1,17 @@
 int red_led =0;
 int yel_led =1;
 int gre_led =2;
-const int keyPin = 5;
-const int ledPin1 = 1;
-const int ledPin2 = 0;
 
 void setup()
 {
   pinMode(red_led, OUTPUT);
   pinMode(yel_led, OUTPUT);
   pinMode(gre_led, OUTPUT);
-  pinMode(keyPin,INPUT);
-  pinMode(ledPin1,OUTPUT);
-  pinMode(ledPin2,OUTPUT);
 }
 
 void loop()
 {
-   if(digitalRead(keyPin) == HIGH) {
-   digitalWrite(ledPin1, HIGH);
-   delay(1000);
-   digitalWrite(ledPin1, LOW);
-   digitalWrite(ledPin2, HIGH);
-   delay(1000);
-   digitalWrite(ledPin2, LOW);}
-  
+   
   digitalWrite(red_led, HIGH);
   digitalWrite(yel_led, LOW);
   digitalWrite (gre_led, LOW); 
@@ -38,5 +25,3 @@ void loop()
   digitalWrite(gre_led, HIGH);
   delay(1000);
 }
-//Первый коммит.
-//Второй коммит.
