30426f4ee24741c0272c1885723cb8119bfb2df1
Кирилл Фролов <55494281+FrolovKir@users.noreply.github.com>
2019-09-25 16:11:06 +0300

    Update Svetofor.ino

--- Svetofor.ino
+++ Svetofor.ino
@@ -1,17 +1,28 @@
 int red_led =0;
 int yel_led =1;
 int gre_led =2;
-
+const int keyPin = 5;
+const int ledPin1 = 1;
+const int ledPin2 = 0;
 
 void setup()
 {
   pinMode(red_led, OUTPUT);
   pinMode(yel_led, OUTPUT);
   pinMode(gre_led, OUTPUT);
+  pinMode(keyPin,INPUT);
+  pinMode(ledPin1,OUTPUT);
+  pinMode(ledPin2,OUTPUT);
 }
 
 void loop()
 {
+   if(digitalRead(keyPin) == HIGH) {
+   digitalWrite(ledPin1, HIGH);
+   delay(1000);
+   digitalWrite(ledPin2, HIGH);
+   delay(1000);}
+  
   digitalWrite(red_led, HIGH);
   digitalWrite(yel_led, LOW);
   digitalWrite (gre_led, LOW); 
@@ -25,4 +36,5 @@ void loop()
   digitalWrite(gre_led, HIGH);
   delay(1000);
 }
-//Первая часть кода , вроде робит
+//Первый коммит.
+//Второй коммит.
